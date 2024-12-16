9669cfbb71a9e52037dea57ca565768cda147d19
info3dcoded@gmail.com <info3dcoded@gmail.com>
2024-12-16 20:27:18 +0000

    Created UploadForm; Edited settings, custom package dependencies and configGenerator; Deleted Form and configGeneratorWeb

---
+++ client_code/UploadForm/__init__.py
@@ -0,0 +1,31 @@
+from ._anvil_designer import UploadFormTemplate
+from anvil import *
+import anvil.server
+
+class UploadForm(UploadFormTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.uploadMethod = 'url'
+
+    # Any code you write here will run before the form opens.
+
+  def file_radio_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    self.uploadMethod = 'file'
+    self.url_radio.selected = False
+    self.file_box.visible = True
+    self.url_box.visible = False
+
+  def url_radio_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    self.uploadMethod = 'url'
+    self.file_radio.selected = False
+    self.url_box.visible = True
+    self.file_box.visible = False
+
+  def upload_submit_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call("upload", method=self.uploadMethod, url=self.url_box.text, file=self.file_box.file)
+
+  
